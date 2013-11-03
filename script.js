dd893382744a98abc44016739f696c419903452d
dzamkov <dzamkov@xiatek.org>
2013-11-03 11:53:04 -0600

    Demonstration of streaming vertex buffer

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ function init() {
 	
 	buffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0]), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, 3 * 6 * 4, gl.STREAM_DRAW);
+	
 	
 	onResize();
 	window.addEventListener('resize', onResize, false);
@@ -24,6 +25,7 @@ function init() {
 	(function animate() {
 		requestAnimationFrame(animate);
 		onRenderFrame();
+		onUpdateFrame(1.0 / 60.0);
 	})();
 }
 
@@ -41,7 +43,6 @@ function onRenderFrame() {
 		
 		var proj = mat4.create();
 		mat4.perspective(45, canvas.width / canvas.height, 0.1, 100.0, proj);
-		//mat4.identity(proj);
 		
 		var view = mat4.create();
 		mat4.lookAt([2, 2, 2], [0, 0, 0], [0, 0, 1], view);
@@ -50,9 +51,18 @@ function onRenderFrame() {
         gl.uniformMatrix4fv(program.view, false, view);
 		
 		var vertex_position;
-		gl.vertexAttribPointer(vertex_position, 2, gl.FLOAT, false, 0, 0);
+		gl.vertexAttribPointer(vertex_position, 3, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(vertex_position);
 		gl.drawArrays(gl.TRIANGLES, 0, 6);
 		gl.disableVertexAttribArray(vertex_position);
 	}
+}
+
+var time = 0;
+function onUpdateFrame(delta) {
+	time += delta;
+	x = 1.0 + Math.cos(time) * 0.5;
+	y = 1.0 + Math.sin(time) * 0.5;
+	z = Math.tan(time / 3.0) * 0.3;
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array([-x, -y, z, x, -y, z, -x, y, z, x, -y, z, x, y, z, -x, y, z]));
 }
